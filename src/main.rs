0d56becf6f64c3ba260f8bb943dc6f24c3dc97c8
Isaac Vitorino Lopes Alves <129021004+ivtrL@users.noreply.github.com>
2023-12-28 00:47:48 -0300

    cargo dependencies updated and added new api route

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ use http_libs::{
     request::Request,
     response::Response,
 };
+use serde::{ Deserialize, Serialize };
+use serde_json;
+
+#[derive(Deserialize, Serialize)]
+struct User {
+    username: String,
+    password: String,
+}
 
 fn hello_handler(_req: Request) -> FutureResponse<'static> {
     let html = "<html><body><h1>Hello, world!</h1></body></html>";
@@ -22,12 +30,34 @@ fn hello_handler(_req: Request) -> FutureResponse<'static> {
     Box::pin(async move { Ok(response) })
 }
 
+fn api_login(_req: Request) -> FutureResponse<'static> {
+    let response = Response {
+        version: String::from("HTTP/1.1"),
+        status_code: 200,
+        status_message: String::from("OK"),
+        headers: {
+            let mut headers = HashMap::new();
+            headers.insert(String::from("Content-Type"), String::from("application/json"));
+            headers
+        },
+        body: {
+            let user = User {
+                username: String::from("Isaac"),
+                password: String::from("123456"),
+            };
+            Some(serde_json::to_string(&user).unwrap())
+        },
+    };
+    Box::pin(async move { Ok(response) })
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let _server = ServerBuilder::new()
         .bind(addr)
         .route(HttpMethod::GET, "/", hello_handler)
+        .route(HttpMethod::GET, "/api", api_login)
         .build()?
         .run().await?;
 
