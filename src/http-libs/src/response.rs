34a6028605d0da24ffe8e72bfb6268524dab8312
Isaac Vitorino Lopes Alves <129021004+ivtrL@users.noreply.github.com>
2023-12-27 23:38:37 -0300

    Update response.rs

--- src/http-libs/src/response.rs
+++ src/http-libs/src/response.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Response {
     pub version: String,
-    pub status_code: String,
+    pub status_code: u16,
     pub status_message: String,
     pub headers: HashMap<String, String>,
     pub body: Option<String>,
