f16f99d4e481cfcb2ee52fd6c465bb8cd315d618
Isaac Vitorino Lopes Alves <129021004+ivtrL@users.noreply.github.com>
2023-12-27 22:17:33 -0300

    Update lib.rs

--- src/http-libs/src/lib.rs
+++ src/http-libs/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![feature(async_fn_in_trait)]
+pub mod http;
+pub mod request;
+pub mod response;
+pub mod server;
